fb548a8dbb68893e8bacdb3be8f3ccabb980c528
Nic Waller <code@nicwaller.com>
2019-12-01 12:21:33 -0800

    day 1, part 2

--- day01/solution.js
+++ day01/solution.js
@@ -2,6 +2,20 @@ var fs = require('fs');
 function fuelForModule(mass) {
     return Math.floor(mass / 3) - 2;
 }
+function fuelForModuleAndFuel(mass, depth) {
+    if (depth === void 0) { depth = 0; }
+    console.log(mass + ", " + depth);
+    if (depth > 15) {
+        throw new Error('call depth');
+    }
+    var partialFuel = Math.floor(mass / 3) - 2;
+    if (partialFuel > 0) {
+        return partialFuel + fuelForModuleAndFuel(partialFuel, depth + 1);
+    }
+    else {
+        return 0;
+    }
+}
 function expectEqual(value, toBe) {
     if (value !== toBe) {
         throw new Error("Expected " + toBe + " but received " + value);
@@ -13,20 +27,26 @@ function test() {
     expectEqual(fuelForModule(14), 2);
     expectEqual(fuelForModule(1969), 654);
     expectEqual(fuelForModule(100756), 33583);
+    expectEqual(fuelForModuleAndFuel(14), 2);
+    expectEqual(fuelForModuleAndFuel(1969), 966);
+    expectEqual(fuelForModuleAndFuel(100756), 50346);
     console.log('All tests passed');
 }
 function main() {
     test();
-    var totalFuel = 0;
+    var totalFuel1 = 0;
+    var totalFuel2 = 0;
     var lines = fs.readFileSync('./puzzle_input').toString().split('\n');
     for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
         var line = lines_1[_i];
         if (line !== '') {
             var mass = parseInt(line);
-            totalFuel += fuelForModule(mass);
+            totalFuel1 += fuelForModule(mass);
+            totalFuel2 += fuelForModuleAndFuel(mass);
         }
         // console.log(line);
     }
-    console.log(totalFuel);
+    console.log(totalFuel1);
+    console.log(totalFuel2);
 }
 main();
--- day01/solution.ts
+++ day01/solution.ts
@@ -4,6 +4,19 @@ function fuelForModule(mass: number): number {
     return Math.floor(mass / 3) - 2;
 }
 
+function fuelForModuleAndFuel(mass: number, depth: number = 0): number {
+    console.log(`${mass}, ${depth}`);
+    if (depth > 15) {
+        throw new Error('call depth')
+    }
+    const partialFuel: number = Math.floor(mass / 3) - 2;
+    if (partialFuel > 0) {
+        return partialFuel + fuelForModuleAndFuel(partialFuel, depth + 1)
+    } else {
+        return 0;
+    }
+}
+
 function expectEqual(value: any, toBe: any) {
     if (value !== toBe) {
         throw new Error(`Expected ${toBe} but received ${value}`);
@@ -15,22 +28,28 @@ function test() {
     expectEqual(fuelForModule(14), 2);
     expectEqual(fuelForModule(1969), 654);
     expectEqual(fuelForModule(100756), 33583);
+    expectEqual(fuelForModuleAndFuel(14), 2);
+    expectEqual(fuelForModuleAndFuel(1969), 966);
+    expectEqual(fuelForModuleAndFuel(100756), 50346);
     console.log('All tests passed');
 }
 
 function main() {
     test();
 
-    let totalFuel = 0;
+    let totalFuel1 = 0;
+    let totalFuel2 = 0;
     const lines = fs.readFileSync('./puzzle_input').toString().split('\n');
     for (const line of lines) {
         if (line !== '') {
             const mass: number = parseInt(line);
-            totalFuel += fuelForModule(mass);
+            totalFuel1 += fuelForModule(mass);
+            totalFuel2 += fuelForModuleAndFuel(mass);
         }
         // console.log(line);
     }
-    console.log(totalFuel);
+    console.log(totalFuel1);
+    console.log(totalFuel2);
 }
 
 main();
